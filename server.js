0211bed1d3d36db997fe0a02723588791a69aa4a
lkukuruku <l.kukuruku@gmail.com>
2020-10-11 17:45:52 +0300

    center login card

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 
-app.use(express.static('dist'))
+app.use(express.static('dist'));
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
