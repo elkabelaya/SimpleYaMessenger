cccfb3095930d1082e584cd0a6f8e6fa39cd4a7a
lkukuruku <l.kukuruku@gmail.com>
2020-10-17 09:40:45 +0300

    fixes on review

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 прототип в фигма https://www.figma.com/file/XZQQ2pJDtcRsNmdnTaN4U3/ya_test_chat?node-id=83%3A49
 
 ## Netlify
-https://app.netlify.com/teams/elkabelaya/sites
+https://eager-tereshkova-2a5c34.netlify.app/
 
 ##Локальный запуск
 npm install && node server.js
--- static/src/js/form_handler.js
+++ static/src/js/form_handler.js
@@ -1,9 +1,21 @@
-function printData(){
-  let formData = new FormData(document.getElementsByTagName("form")[0]);
-  let result = {};
-  for (var key of formData.keys()) {
+document.addEventListener("DOMContentLoaded", ()=>{
+  let form;
+  for (const i in Object.values(document.forms)){
+    form = document.forms[i];
+    form.addEventListener("submit", printData);
+
+  }
+});
+
+function printData(event){
+  event.preventDefault();
+
+  const formData = new FormData(event.currentTarget);
+  const result = {};
+  for (const key of formData.keys()) {
 		result[key] = formData.get(key);
 	}
+
   console.log(result);
 
 }
